32f4df34805a010cefc1ce3a5efd11703cc114ca
Caitlin <caitlinsweeneylmt@gmail.com>
2018-05-16 13:23:45 -0700

    need to proof read and test

--- lab-7-sweeney/index.js
+++ lab-7-sweeney/index.js
@@ -20,3 +20,91 @@ function bodyParser(req, cb){
     cb(null,'{}');
   }
 }
+
+const server = http.createServer((req, res) =>{
+  req.url = url.parse(req.url);
+  req.url.query = querystring.parse(req.url.query);
+  console.log('url', req.url.query);
+  if(req.method === 'GET'){
+    if(req.url.pathname === '/'){
+      res.writeHead(200, {'Content-Type':'text/html'});
+      res.write(
+        <html>
+      <head>
+        <title> COWSAY </title>
+        </head>
+      <body>
+        <header>
+          <nav>
+            <ul>
+              <li><a href = "/cowsay">COWSAY</a></li>
+              </ul>
+            </nav>
+          <header>
+          <main>
+            interactive server
+          </main>
+        </body>
+      </html>
+      );
+      res.end();
+      }
+      if(req.url.pathname === '/cowsay'){
+        console.log('getting to /cowsay');
+        let input;
+        if (req.url.query.text){
+          input = req.url.query.text;
+          console.log('input', input);
+        }
+        else{
+          console.log('null input');
+          input = 'Tell me something to say';
+        }
+        res.writeHead(200,{
+          'Content-Type':'text/html'
+        });
+        res.write(
+          <html>
+        <head>
+          <title> COWSAY</title>
+        </head>
+        <body>
+          <h1> COWSAY </h1>
+          <pre>
+            ${cowsay.say({text:input})}
+          </pre>
+        </body>
+      </html>
+       );
+       res.end();
+
+      }
+    }
+    if(req.method === 'POST'){
+      console.log('getting to post');
+      if(req.url.pathname === 'api/cowsay'){
+        console.log('getting to pathname');
+        bodyParser(req, (err,body) => {
+          body = JSON.parse(body);
+          if(body.text){
+            cowsay.say({text:input});
+            rew.writeHead(200, {'Content-Type':'application/json'});
+            res.write(JSON.stringify({content: cowsay.say({text: input})}));
+            res.end();
+          }
+          else{
+            let message = JSON.stringify({
+              error : 'invalid request: text query required',
+            });
+            res.writeHead(400, {'Content-Type':'application/json'});
+            res.write(message);
+            res.end();
+          }
+        });
+      }
+    }
+  });
+
+  server.listen(PORT, () =>{
+    console.log('listening on port'+PORT);
+  });
